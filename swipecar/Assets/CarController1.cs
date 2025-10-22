1eff8d3cc55a7f42914ec80e92e990615a290ccb
jinxiaol <wesleywu314@gmail.com>
2025-10-22 14:44:25 +0800

    1022

--- swipecar/Assets/CarController1.cs
+++ swipecar/Assets/CarController1.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class CarController1 : MonoBehaviour
 {
     float speed = 0;
+    Vector2 startPos;
     void Start()
     {
         Application.targetFrameRate = 60;
@@ -14,7 +15,13 @@ public class CarController1 : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            this.speed = 0.2f;
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+            this.speed = swipeLength / 500.0f;
         }
         transform.Translate(this.speed, 0, 0);
         this.speed *= 0.98f;
